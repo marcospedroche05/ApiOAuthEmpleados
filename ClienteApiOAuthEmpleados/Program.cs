171ceca403d9e65d54dce01eb060112a0ce8f244
marcospedroche05 <marcospedroche62@gmail.com>
2026-04-07 12:56:45 +0200

    PUCH

--- ClienteApiOAuthEmpleados/Program.cs
+++ ClienteApiOAuthEmpleados/Program.cs
@@ -4,7 +4,20 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net.Http.Headers;
 using System.Text;
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Cliente Api OAuth");
+Console.WriteLine("Introduzca apellido");
+string apellido = Console.ReadLine();
+Console.WriteLine("Id del empleado (Password)");
+string password = Console.ReadLine();
+string response = await GetTokenAsync(apellido, password);
+Console.WriteLine(response);
+Console.WriteLine("------------------------------");
+Console.WriteLine("Introduzca ID del empleado a buscar");
+string idEmpleado = Console.ReadLine();
+string datos = await FindEmpleadoAsync(int.Parse(idEmpleado), response);
+Console.WriteLine(datos);
+
 
 //PARA CREAR METODOS EN PROGRAM, DEBEMOS HACERLO STATIC
 static async Task<string> GetTokenAsync(string user, string pass)
@@ -35,5 +48,29 @@ static async Task<string> GetTokenAsync(string user, string pass)
         {
             return "Peticion incorrecta: " + response.StatusCode;
         }
+    }
+}
+
+static async Task<string> FindEmpleadoAsync(int idEmpleado, string token)
+{
+    string urlApi = "https://apioauthempleadosmarcos-c9fgbdh0bdbja8cj.francecentral-01.azurewebsites.net/";
+    using (HttpClient client = new HttpClient())
+    {
+        string request = "api/empleados/" + idEmpleado;
+        client.BaseAddress = new Uri(urlApi);
+        client.DefaultRequestHeaders.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //LO UNICO DE NOVEDAD ES INCLUIR authorization con bearer TOKEN DENTRO DEL HEADER
+        client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+        HttpResponseMessage response = await client.GetAsync(request);
+        if(response.IsSuccessStatusCode == true)
+        {
+            string data = await response.Content.ReadAsStringAsync();
+            return data;
+        } else
+        {
+            return "Error de algo: " + response.StatusCode;
+        }
+
     }
 }
